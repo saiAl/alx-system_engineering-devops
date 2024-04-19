bb5c37d819bb770fd58e20f8b3968d57a4f2384d
Sai <ojiip95@gmail.com>
2024-04-19 14:44:19 +0200

    0x0A: add another package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,5 +3,11 @@
 package { 'install flask':
   ensure   => 'installed',
   name     => 'flask==2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'install werkzeug':
+  ensure   => 'installed',
+  name     => 'Werkzeug==2.1.1',
+  provider => 'pip3',
 }
