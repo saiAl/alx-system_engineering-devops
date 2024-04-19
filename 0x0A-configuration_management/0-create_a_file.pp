717ed27f1db66ad31288c5fbd100a56b894da75e
Sai <ojiip95@gmail.com>
2024-04-19 14:32:40 +0200

    0x0A: task 0, fix indentation errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { 'create file':
-  path => '/tmp/school',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
