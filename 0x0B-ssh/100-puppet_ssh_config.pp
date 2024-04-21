484730054974a63fb5fad51bb5747462af3e19c7
Sai <ojiip95@gmail.com>
2024-04-21 11:32:55 +0200

    0x0B: task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# configuration of ssh
+
+file { 'disable password':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no'
+}
+
+file { 'identity file':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    =>     IdentityFile ~/.ssh/school'
+}
