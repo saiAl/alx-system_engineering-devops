b63df8b0ce9aeb64d6a40b16cf85a28c176ccf00
sai <ojiip95@gmail.com>
2024-05-05 13:10:18 +0200

    add the puppet maifist

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,34 @@
+class nginx {
+
+  include puppet::nginx
+
+  package { 'nginx': ensure => present }
+
+  nginx::vhost { 'default':
+    ensure => present,
+    port => 80,
+    server_name => ['localhost'],
+    www_root => '/var/www/html',
+    content => <<EOF
+    server {
+        listen 80;
+        server_name localhost;
+
+        location / {
+          root /var/www/html;
+          index index.html index.htm;
+          try_files $uri $uri/ /index.html;
+        }
+
+        location /redirect_me {
+          return 301 $scheme://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+    }
+EOF
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enabled => true,
+  }
+}
