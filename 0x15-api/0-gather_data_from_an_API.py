9432cdf8b3332b8aed0e9f7cead59474fb30da85
sai <ojiip95@gmail.com>
2024-05-20 19:30:25 +0200

    0x15: task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" 0. Gather data from an API """
+import requests
+import sys
+
+
+def fetch(url=None):
+    """ fetch a information from endpoint"""
+    r = requests.get(url)
+    return r.json()
+
+
+def main():
+    """ information about employee TODO list progress."""
+    emplyeeId = int(sys.argv[1])
+    done = 0
+    total = 0
+    completed = []
+
+    tasks = fetch("https://jsonplaceholder.typicode.com/todos")
+    user = fetch(f"https://jsonplaceholder.typicode.com/users/{emplyeeId}")
+
+    name = user.get("name")
+    for task in tasks:
+        if task.get("userId") == emplyeeId:
+            if task.get("completed") is True:
+                completed.append(task.get("title"))
+                done += 1
+            total += 1
+
+    print(f"Employee {name} is done with tasks({done}/{total}")
+    for x in completed:
+        print(f"\t{x}")
+
+
+main()
