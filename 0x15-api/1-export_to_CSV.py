6012509c32060bdf4544b87df96450b2de242bdb
sai <ojiip95@gmail.com>
2024-05-21 04:37:25 +0200

    0x15: task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""1. Export to CSV"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    def fetch(url=None):
+        """fetch a information from endpoint"""
+        r = requests.get(url)
+        return r.json()
+
+    emplyeeId = int(sys.argv[1])
+    tasks = fetch(
+            f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos"
+            )
+    user = fetch(
+            f"https://jsonplaceholder.typicode.com/users/{emplyeeId}"
+            )
+
+    with open(f"{emplyeeId}.csv", 'w', newline='') as file:
+        data = csv.writer(
+                file, delimiter=',', quoting=csv.QUOTE_ALL
+                )
+        for task in tasks:
+            data.writerow(
+                    [emplyeeId, user.get("name"),
+                        task.get("completed"), task.get("title")]
+                    )
