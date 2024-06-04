9ca018bec56ee40f30b512703d7d8d5aaf38d6e7
sai <ojiip95@gmail.com>
2024-06-04 22:41:51 +0200

    0x16: use try except statment to handles errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -21,8 +21,8 @@ def top_ten(subreddit):
         response = requests.get(
                 url, headers=headers, allow_redirects=False
                 )
-        if response.status_code != 200:
-            print("None")
-        else:
+        try:
             for x in response.json()["data"]["children"]:
                 print(x["data"]["title"])
+        except Exception as e:
+            print("None")
