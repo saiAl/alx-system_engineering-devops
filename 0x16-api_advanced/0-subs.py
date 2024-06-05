e9ad1ce7a8fc4bedaa95db3be0e0a361512282ba
sai <ojiip95@gmail.com>
2024-06-05 18:22:46 +0200

    0x16: task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -25,6 +25,7 @@ def number_of_subscribers(subreddit):
     response = requests.get(
             url, headers=headers, allow_redirects=False
             )
+
     if response.status_code != 200:
         return 0
     else:
