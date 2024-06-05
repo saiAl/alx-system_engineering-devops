543d273c785307079e81765e6f7ff27077a97a78
sai <ojiip95@gmail.com>
2024-06-05 19:05:06 +0200

    fix error in config

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix internal error
 
-file {'create missing file':
-  ensure => 'present',
-  path   => '/var/www/html/index.html',
+exec { 'fix-internal':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
