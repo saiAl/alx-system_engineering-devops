959f4e619798d122be08356f4bf1346babebbc83
sai <ojiip95@gmail.com>
2024-06-06 19:39:57 +0200

    add header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,19 +14,17 @@ def number_of_subscribers(subreddit):
             otherwise return 0.
     """
 
-    if not isinstance(subreddit, str):
-        return 0
-
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-            "User-Agent": "Mozilla/5.0"
+            "User-Agent": "Mozilla/5.0",
+            "x-requested-with": "XMLHttpRequest"
             }
 
     response = requests.get(
             url, headers=headers, allow_redirects=False
             )
 
-    if response.status_code != 200:
-        return 0
-    else:
+    if response.status_code == 200:
         return response.json()["data"]["subscribers"]
+
+    return 0
